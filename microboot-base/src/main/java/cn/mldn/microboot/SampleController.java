bd87aba96124ee636e2b52dfb571652a0317deb1
hbinglin <a416629210@163.com>
2019-02-20 11:14:49 +0800

    1、完成路径访问控制练习 2、完成使用内置对象练习 3、完成项目打包部署练习

--- microboot-base/src/main/java/cn/mldn/microboot/SampleController.java
+++ microboot-base/src/main/java/cn/mldn/microboot/SampleController.java
@@ -1,17 +1,30 @@
 package cn.mldn.microboot;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@Controller
-@EnableAutoConfiguration
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SampleController {
+
     @RequestMapping("/")
-    @ResponseBody
     public String home() {
         return "www.mldn.cn";
     }
 
+    @RequestMapping("/echo")
+    public String echo(String msg) {
+        return msg;
+    }
+
+    @RequestMapping("/object")
+    public void object(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("客戶端IP地址：" + request.getRemoteAddr());
+        System.out.println("本地地址：" + request.getLocalAddr());
+        System.out.println("客户端响应编码：" + response.getCharacterEncoding());
+        System.out.println("获取sessionID：" + request.getSession().getId());
+        System.out.println("获取真实路径：" + request.getServletContext().getRealPath("/upload/"));
+    }
 }
